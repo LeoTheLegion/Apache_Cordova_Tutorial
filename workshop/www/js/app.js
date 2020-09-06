707df49c268a5e031edb86b68190cd2fd782349d
LeoTheLegion <mmena2017@fau.edu>
2020-09-06 19:36:14 -0400

    tut complete

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -4,12 +4,24 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
-    
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+
     var service = new EmployeeService();
-    
+    var slider = new PageSlider($('body'));
+
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
-    });
+        router.addRoute('', function() {
+            slider.slidePage(new HomeView(service).render().$el);
+        });
+      
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+                slider.slidePage(new EmployeeView(employee).render().$el);
+            });
+        });
+      
+        router.start();
+      });
 
     /* --------------------------------- Event Registration -------------------------------- */
     
